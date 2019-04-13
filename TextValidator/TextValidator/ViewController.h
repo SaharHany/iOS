a39d8e47d412a06443a2e03b92388f5801867d55
SaharHany <36588837+SaharHany@users.noreply.github.com>
2019-04-13 15:16:29 +0200

    validator

---
+++ TextValidator/TextValidator/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  TextValidator
+//
+//  Created by JETS Mobile Lab on 4/13/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *text;
+@property (weak, nonatomic) IBOutlet UILabel *validate;
+
+- (IBAction)Validate:(UIButton *)sender;
+
+@end
+
---
+++ TextValidator/TextValidator/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  TextValidator
+//
+//  Created by JETS Mobile Lab on 4/13/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (IBAction)Validate:(UIButton *)sender {
+}
+@end
